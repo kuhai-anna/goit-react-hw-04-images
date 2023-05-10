51cea87bfaa15562d4be7d432db60f31e77883db
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-10 22:43:44 +0300

    Update App.jsx
    1. Add state machine
    2. Add "Load more" button rendering when the status is "resolved"
    3. Remove handleSelectedImg func

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { Section } from './Section/Section';
 import { Modal } from './Modal/Modal';
 import { Button } from './Button/Button';
 import { ModalImage } from './App.styled';
+import { Status } from 'constants/status';
 
 export const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -13,21 +14,19 @@ export const App = () => {
   const [totalHits, setTotalHits] = useState(null);
   const [selectedImg, setSelectedImg] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [status, setStatus] = useState(Status.IDLE);
 
   // передача пошукового запиту при сабміті форми
   const handleFormSubmit = ({ searchQuery, page }) => {
     setSearchQuery(searchQuery);
     setPage(page);
-  };
-
-  // передача посилання на активну картинку при кліку на картинку
-  const handleSelectedImg = largeImageURL => {
-    setSelectedImg(largeImageURL);
+    setStatus(Status.PENDING);
   };
 
   //прередача загальної кількості знайдених картинок при отриманні результату запиту
-  const viewLoadMoreBtn = totalHits => {
+  const viewLoadMoreBtn = (totalHits, status) => {
     setTotalHits(totalHits);
+    setStatus(status);
   };
 
   // відкриття та закриття модалки
@@ -38,31 +37,80 @@ export const App = () => {
   // збільшення номеру сторінки при кліку на кнопку
   const loadMoreBtnClick = () => {
     setPage(prevState => prevState + 1);
+    setStatus(Status.PENDING);
   };
 
   const notLastPage = page < Math.ceil(totalHits / 12);
 
-  return (
-    <>
-      <Searchbar onSubmit={handleFormSubmit} />
-      <Section>
-        <ImageGallery
-          searchQuery={searchQuery}
-          page={page}
-          onClick={toggleModal}
-          onSelectImage={handleSelectedImg}
-          viewLoadMoreBtn={viewLoadMoreBtn}
-        />
-        {showModal && (
-          <Modal onClose={toggleModal}>
-            <ModalImage src={selectedImg} alt="" />
-          </Modal>
-        )}
+  if (status !== 'resolved') {
+    return (
+      <>
+        <Searchbar onSubmit={handleFormSubmit} />
+        <Section>
+          <ImageGallery
+            searchQuery={searchQuery}
+            page={page}
+            onClick={toggleModal}
+            onSelectImage={setSelectedImg}
+            viewLoadMoreBtn={viewLoadMoreBtn}
+          />
+          {showModal && (
+            <Modal onClose={toggleModal}>
+              <ModalImage src={selectedImg} alt="" />
+            </Modal>
+          )}
+        </Section>
+      </>
+    );
+  }
 
-        {totalHits > 12 && notLastPage && <Button onClick={loadMoreBtnClick} />}
-      </Section>
-    </>
-  );
+  if (status === 'resolved') {
+    return (
+      <>
+        <Searchbar onSubmit={handleFormSubmit} />
+        <Section>
+          <ImageGallery
+            searchQuery={searchQuery}
+            page={page}
+            onClick={toggleModal}
+            onSelectImage={setSelectedImg}
+            viewLoadMoreBtn={viewLoadMoreBtn}
+          />
+          {showModal && (
+            <Modal onClose={toggleModal}>
+              <ModalImage src={selectedImg} alt="" />
+            </Modal>
+          )}
+
+          {totalHits > 12 && notLastPage && (
+            <Button onClick={loadMoreBtnClick} />
+          )}
+        </Section>
+      </>
+    );
+  }
+
+  // return (
+  //   <>
+  //     <Searchbar onSubmit={handleFormSubmit} />
+  //     <Section>
+  //       <ImageGallery
+  //         searchQuery={searchQuery}
+  //         page={page}
+  //         onClick={toggleModal}
+  //         onSelectImage={setSelectedImg}
+  //         viewLoadMoreBtn={viewLoadMoreBtn}
+  //       />
+  //       {showModal && (
+  //         <Modal onClose={toggleModal}>
+  //           <ModalImage src={selectedImg} alt="" />
+  //         </Modal>
+  //       )}
+
+  //       {totalHits > 12 && notLastPage && <Button onClick={loadMoreBtnClick} />}
+  //     </Section>
+  //   </>
+  // );
 };
 
 // export class App extends Component {
