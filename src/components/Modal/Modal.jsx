56d6fcacce7a371e2ece81e7c89e7b148ea8f885
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-11 17:48:23 +0300

    Remove comments from the Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -40,38 +40,3 @@ Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   children: PropTypes.any.isRequired,
 };
-
-// export class Modal extends Component {
-//   static = {
-//     onClose: PropTypes.func.isRequired,
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.hendleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.hendleKeyDown);
-//   }
-
-//   hendleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   hendleBackdropClick = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     return createPortal(
-//       <Overlay onClick={this.hendleBackdropClick}>
-//         <ModalContent>{this.props.children}</ModalContent>
-//       </Overlay>,
-//       modalRoot
-//     );
-//   }
-// }
